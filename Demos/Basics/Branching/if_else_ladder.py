d6ff0bb31530d8438e9192054fa100dfde639257
Ghanashyam Kumbhar <shyamkumbhar9816@gmail.com>
2026-09-05 22:22:59 +0530

    Delete Demos/Basics directory

--- Demos/Basics/Branching/if_else_ladder.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Enter Number:'))
-
-if (num == 0):
-    print('Number is neutral.')
-elif(num > 0):
-    print('Number is positive.')
-else:
-    print('Number is negative.')
-      
